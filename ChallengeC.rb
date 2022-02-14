d7e24529604ef72345183da97cb8506381579c04
angle21 <99356263+angle21@users.noreply.github.com>
2022-02-14 11:39:18 -0800

    Update ChallengeC.rb

--- ChallengeC.rb
+++ ChallengeC.rb
@@ -1,1 +1,41 @@
+use_bpm 160
+use_synth :saw
 
+
+
+define :stranger_things_theme do
+  play :c2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  # Second set of eigth notes
+  play :c3
+  sleep 0.5
+  play :b2
+  sleep 0.5
+  play :g2
+  sleep 0.5
+  play :e2
+  sleep 0.5
+  
+end
+
+with_fx :tremolo do
+  stranger_things_theme
+end
+
+with_fx :flanger do
+  stranger_things_theme
+end
+
+with_fx :whammy do
+  stranger_things_theme
+end
+
+with_fx :wobble do
+  stranger_things_theme
+end
